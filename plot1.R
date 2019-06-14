d23fc3b304efdcdfe11d624e878e5bab2964bd1d
Toby Wright <49879024+tobyjameswright@users.noreply.github.com>
2019-06-14 08:03:27 +0200

    Add files via upload

---
+++ plot1.R
@@ -0,0 +1,15 @@
+library(dplyr)
+
+#read in data
+mydata <- read.csv(file= "household_power_consumption.txt", header=T, sep = ";", stringsAsFactors = F)
+#set date to date
+mydata$Date <- as.Date(mydata$Date, "%d/%m/%Y")
+#subset data to two days
+data <- subset(mydata, mydata$Date == "2007-02-01" | mydata$Date == "2007-02-02")
+
+mydata$Global_active_power <- as.numeric(mydata$Global_active_power)
+#plot histogram
+with(data, hist(Global_active_power, col="red", xlab = "Global Active Power (kilowatts)", main="Global Active Power"))
+#copy to png
+dev.copy(device=png)
+dev.off()
