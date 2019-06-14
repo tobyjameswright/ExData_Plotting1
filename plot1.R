e60a9a9a7492dee3c87b78beb2cfd495e2df7ca4
Toby Wright <49879024+tobyjameswright@users.noreply.github.com>
2019-06-14 08:08:20 +0200

    Add files via upload

--- plot1.R
+++ plot1.R
@@ -6,7 +6,7 @@ mydata <- read.csv(file= "household_power_consumption.txt", header=T, sep = ";",
 mydata$Date <- as.Date(mydata$Date, "%d/%m/%Y")
 #subset data to two days
 data <- subset(mydata, mydata$Date == "2007-02-01" | mydata$Date == "2007-02-02")
-
+#set Global Active power to numeric
 mydata$Global_active_power <- as.numeric(mydata$Global_active_power)
 #plot histogram
 with(data, hist(Global_active_power, col="red", xlab = "Global Active Power (kilowatts)", main="Global Active Power"))
